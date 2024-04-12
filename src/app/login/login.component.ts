b75586cba75fd8cb9e7aa69c31cff7b796b4f69a
Joel Munuera Marrero <joel.munu@gmail.com>
2024-04-12 11:16:35 +0100

    Se le ha añadido funcionalidad al login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginData } from '../models/login.interface';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm = {
+    username: "",
+    password: ""
+  }
+
+  constructor(private api: ApiService, private router: Router) {}
+
+  ngOnInit(): void {
+  }
+
+  Login(form: LoginData) {
+    this.api.Login(form).subscribe(data =>  {
+      console.log(data);
+    });
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -4,18 +4,23 @@ import { Observable } from 'rxjs';
 
 import { RegisterData } from '../models/register.interface';
 import { ResponseData } from '../models/response.interface';
+import { LoginData } from '../models/login.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  
+  private registerUrl = 'http://localhost:8888/auth/register';
+  private loginUrl = 'http://localhost:8888/auth/login';
 
-    private registerUrl = 'http://localhost:8888/auth/register';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
+  Register(form: RegisterData): Observable<ResponseData> {
+    return this.http.post<ResponseData>(this.registerUrl, form);
   }
 
-  Register(form:RegisterData): Observable<ResponseData> {
-    return this.http.post<ResponseData>(this.registerUrl, form);
+  Login (form: LoginData): Observable<ResponseData> {
+    return this.http.post<ResponseData>(this.loginUrl, form);
   }
 }
